8362b80706687e0646af6f8f14386ac66f4d71db
amryuossef889-rgb <amryuossef889@gmail.com>
2026-09-26 08:44:47 +0300

    wire recording DAO into application repository

--- app/src/main/java/com/dokstudio/obs/DokStudioApplication.kt
+++ app/src/main/java/com/dokstudio/obs/DokStudioApplication.kt
@@ -4,5 +4,5 @@ import androidx.room.Room
 import com.dokstudio.obs.data.AppDatabase
 import com.dokstudio.obs.data.SceneRepository
 class DokStudioApplication:Application(){ lateinit var database:AppDatabase; lateinit var repository:SceneRepository
- override fun onCreate(){super.onCreate();database=Room.databaseBuilder(this,AppDatabase::class.java,"dokstudio.db").fallbackToDestructiveMigration().build();repository=SceneRepository(database.sceneDao(),database.sourceDao())}
+ override fun onCreate(){super.onCreate();database=Room.databaseBuilder(this,AppDatabase::class.java,"dokstudio.db").fallbackToDestructiveMigration().build();repository=SceneRepository(database.sceneDao(), database.sourceDao(), database.recordingDao())}
 }
